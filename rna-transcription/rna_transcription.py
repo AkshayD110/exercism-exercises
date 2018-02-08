4c4e9a7f5225371614857e75b3fb589c7b18afa9
AkshayD110 <akshayd110@gmail.com>
2018-02-09 00:29:38 +0530

    comments added

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,9 +1,11 @@
 def to_rna(dna_strand):
     list_of_char = []
     list_of_char = dna_strand
+    #check for all the invalid entries first
     for i in list_of_char:
         if i not in ('C','G','T','A'):
             raise ValueError("Invalid nucliotide")
+    #use translate function to replace text
     trans = str.maketrans('GCTA', 'CGAU')
     newString = (dna_strand.translate(trans))
     return newString
