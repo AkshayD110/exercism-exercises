59ac3c18c0ceee50d36ae3661afe3a5670d5c26a
AkshayD110 <akshayd110@gmail.com>
2018-02-09 00:27:16 +0530

    changed Login

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,15 +1,12 @@
-import re
 def to_rna(dna_strand):
-    rna_list = []
-    dna_to_rna = {'G':'C',
-                  'C':'G',
-                  'T':'A',
-                  'A':'U'
-                  }
-    for n in dna_strand:
-        try:
-            rna_list.append(dna_to_rna[n])
-        except:
-            raise ValueError("No such nucleotide")
-    print(''.join(rna_list))
-    return ''.join(rna_list)
+    list_of_char = []
+    list_of_char = dna_strand
+    for i in list_of_char:
+        if i not in ('C','G','T','A'):
+            raise ValueError("Invalid nucliotide")
+    trans = str.maketrans('GCTA', 'CGAU')
+    newString = (dna_strand.translate(trans))
+    return newString
+
+
+
