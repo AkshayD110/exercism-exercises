2d69d42ea19c154f1d2e92e2c94a316cc55c7269
AkshayD110 <akshayd110@gmail.com>
2018-02-08 23:34:31 +0530

    changed Login

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,7 +1,15 @@
+import re
 def to_rna(dna_strand):
-    check_strand=dna_strand
-    check_strand.replace('A','U')
-    check_strand.replace('C','G')
-    check_strand.replace('G','C')
-    check_strand.replace('T','A')
-    return check_strand
+    rna_list = []
+    dna_to_rna = {'G':'C',
+                  'C':'G',
+                  'T':'A',
+                  'A':'U'
+                  }
+    for n in dna_strand:
+        try:
+            rna_list.append(dna_to_rna[n])
+        except:
+            raise ValueError("No such nucleotide")
+    print(''.join(rna_list))
+    return ''.join(rna_list)
