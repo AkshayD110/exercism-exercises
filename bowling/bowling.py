9f95babc8fe6cff353a6fd9a36c1b6cab207036b
AkshayD110 <akshayd110@gmail.com>
2018-03-31 11:22:02 +0530

    initial commit

--- bowling/bowling.py
+++ bowling/bowling.py
@@ -1,16 +1,9 @@
-
-
 class BowlingGame(object):
     def __init__(self):
         pass
 
     def roll(self, pins):
-        if(pins>=1):
-            print("Array")
-        else:
-            print("int")
+        pass
 
     def score(self):
         pass
-
-#check
